94da34c05268f648ea67ec3de4285ec1cb5dcb2e
Jake <145676328+Seagullls@users.noreply.github.com>
2023-10-12 09:59:40 -0400

    Update ROMtb.v

--- romTest.v
+++ romTest.v
@@ -1,4 +1,26 @@
-module romTest();
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/12/2023 09:10:37 AM
+// Design Name: 
+// Module Name: ROMtb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ROMtb();
 
 //input[31:0] addr;
 //input enable, clk;
@@ -9,10 +31,11 @@ reg [31:0] addr;
 reg enable, clk;
 
 //instant
-rom testRom(addr, enable, ck, out);
+ROM testRom(addr, enable, clk, out);
 
 always begin
-	#5 clk <= ~clk;
+	#5; clk <= 0;
+	#5; clk <= 1;
 end
 
 initial begin
@@ -22,27 +45,27 @@ addr <= 0;
 enable <= 1;
 #5;
 
-enable <= 0;
+//enable <= 0;
 #5;
 //second word
 enable <= 1;
 addr <= 4;
 #5;
 
-enable <= 0;
+//enable <= 0;
 #5;
 //third word
 enable <= 1;
 addr <=8;
 #5;
 
-enable <= 0;
+//enable <= 0;
 #5;
 //fourth word
 enable <= 1;
 addr <= 12;
 #5;
-enable <= 0;
+//enable <= 0;
 end
 
 endmodule
