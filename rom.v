d651b62c311f04cf8aa86eb3aad9458a8062993c
Jake <145676328+Seagullls@users.noreply.github.com>
2023-10-12 09:58:33 -0400

    Update ROM.v

--- rom.v
+++ rom.v
@@ -1,4 +1,26 @@
-module rom(addr, enable, clk, out);
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/12/2023 08:25:36 AM
+// Design Name: 
+// Module Name: ROM
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ROM(addr, enable, clk, out);
 
 reg[7:0] storage [1023:0];
 
@@ -23,10 +45,10 @@ initial begin
 			
 	end //initial end
 
-always @(enable || addr) begin
+always @(posedge clk) begin
 //reading
 	if (enable && addr <= 1020) begin
-		out <= {storage[addr + 3], storage[addr + 2], storage[addr + 1], storage[addr]};
+		out = {storage[addr + 3], storage[addr + 2], storage[addr + 1], storage[addr]};
 	end
 
 	//the addr being above 124 will overflow, and i'm not sure how that would affect anything.
