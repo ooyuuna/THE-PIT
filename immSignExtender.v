5e8305dbbc96a772b857d14f20d19419358a7699
ooyuuna <145676146+ooyuuna@users.noreply.github.com>
2023-12-07 08:58:02 -0500

    Create immSignExtender.v

---
+++ immSignExtender.v
@@ -0,0 +1,26 @@
+module immSignExtender(clk, flag, imm, out);
+input clk;
+input flag; //this is your "ImmSel" flag
+input [11:0] imm;
+output reg [31:0] out;
+
+
+
+always @(posedge clk) begin
+if (flag) begin
+	if (imm[11]) begin //FIll with 1's
+		out <= {20'b11111111111111111111, imm[11:0]};
+	end
+	else begin //fill with 0's
+	out <= {20'b00000000000000000000, imm[11:0]};
+			// {20'b11111111111111111111, imm[11:0]};
+	end
+	
+	
+end
+
+
+
+end
+
+endmodule
