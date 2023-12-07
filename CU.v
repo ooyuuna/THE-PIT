b57d9aafd670ee375a90820e5e72f8ade96465d5
ooyuuna <145676146+ooyuuna@users.noreply.github.com>
2023-12-07 09:21:21 -0500

    Update CU.v

--- CU.v
+++ CU.v
@@ -9,19 +9,47 @@ output reg ImmSel,
 output reg BSel,
 output reg BrUn,
 output reg ASel,
-output reg PCSel
+output reg PCSel,
+output reg WBSel,
+output reg MemRW
     );
     //opcode
 always @ (posedge clk)begin
 
     case (I[6:0])
-    0110011: begin //R TYPE  JAKE IF THIS BREAKS MAKE IT ACTUALLY BINARY
+	 7'b0000011: begin//LW
+	 wEn <= 0; //stay 0
+    ImmSel <= 1;
+    BSel <= 1;
+    BrUn <= 0;
+    ASel <= 0; //control for branch comparator
+    PCSel <= 0; //will only be one for branching when branch taken.
+	 WBSel <= 0; //picking from dmem; (doesn't matter here)
+	 MemRW <= 0; //  reading from mem
+	  ALUop <= 4'b1001; //add
+	 end
+	 
+	 7'b0100011: begin//SW
+	wEn <= 0; //stay 0
+    ImmSel <= 1;
+    BSel <= 1;
+    BrUn <= 0;
+    ASel <= 0; //control for branch comparator
+    PCSel <= 0; //will only be one for branching when branch taken.
+	 WBSel <= 0; //picking from dmem;
+	 MemRW <= 1; //  writing into dmem
+	 ALUop <= 4'b1001; // add
+	 end
+	 
+    7'b0110011: begin //R TYPE  JAKE IF THIS BREAKS MAKE IT ACTUALLY BINARY
     wEn <= 0; //a delay will need to be written for this to enable for writeback
     ImmSel <= 0;
     BSel <= 0;
     BrUn <= 0;
     ASel <= 0; //control for branch comparator
     PCSel <= 0; //will only be one for branching when branch taken.
+	 WBSel <= 1; //picking from ALU;
+	 MemRW <= 0; // 0 is read
 	 
 		case (I[14:12]) //ALUOP stuff
 		3'b100: ALUop <= 4'b0010; //xor
@@ -45,14 +73,15 @@ always @ (posedge clk)begin
 	 
     end
      
-     0010011: begin //I TYPE
+     7'b0010011: begin //I TYPE
     wEn <= 0; //a delay will need to be written for this to enable for writeback
     ImmSel <= 1;
     BSel <= 1;//one when imm only
     BrUn <= 0; //always zero
     ASel <= 0; //control for branch comparator
     PCSel <= 0; //will only be one for branching when branch taken.
-	 
+	 WBSel <= 1; //picking from ALU;
+	 MemRW <= 0; // 0 is read
 	 
 	 case (I[14:12])
 		3'b100: ALUop <= 4'b0010; //xor
@@ -67,13 +96,15 @@ always @ (posedge clk)begin
 	 
     end
     
-    1100011:begin //branching
+    7'b1100011:begin //branching
     wEn <= 0; 
     ImmSel <= 1;
     BSel <= 1;//one when imm only
     BrUn <= 0; //always zero
     ASel <= 1; //control for branch comparator
     ALUop <= 4'b1001; //addition for branching.
+	 WBSel <= 1; //picking from ALU;
+	 MemRW <= 0; // 0 is read
         case (I[14:12])
         000:begin //equal
         if (BrEq)
@@ -83,7 +114,7 @@ always @ (posedge clk)begin
         PCSel <=0;
         end
         
-        001:begin //not equal
+        001:begin //not equal //is this number right i mightve hypothetically deleted it.
         if (!BrEq)
         PCSel <= 0;
         
